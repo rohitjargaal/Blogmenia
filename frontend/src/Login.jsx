e67eb963b4d311b797f2b386216cbed67422f94a
Rohitjargaal <rohitjargaal@gmail.com>
2025-08-29 10:09:15 +0530

    responsive update

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -9,19 +9,18 @@ import Loader from "./compnents/Loader.jsx";
 function Login() {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
-  let [issumbit, setisSubmit] = useState(false)
-    const [isLoading, setIsLoading] = useState(false);
-  
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const navigate = useNavigate()
 
   function Loginhandle(e) {
     e.preventDefault();
-    setisSubmit(true)
+    setIsLoading(true)
     axios.post(`${Backendapi}/users/login`, { email, password })
       .then((res) => {
         if (res.data.success) {
-          localStorage.setItem("tokenkey",res.data.token)
+          localStorage.setItem("tokenkey", res.data.token)
           navigate("/home");
           toast.success(res.data.message);
         }
@@ -32,37 +31,28 @@ function Login() {
   }
   return (
     <>
-        {isLoading ? <Loader /> :
-      (
-            <div className="loginpage">
-      <img src="/media/images/login.png" alt="" />
-      <form onSubmit={Loginhandle}>
-        <h1 style={{ textAlign: "center", marginBottom: "1rem" }}>Login In Blogmenia</h1>
-        <div className="input-container">
-          <label >Email</label>
-          <input type="text" onChange={(e) => { setEmail(e.target.value) }} />
-        </div>
-        <div className="input-container">
-          <label>Password</label>
-          <input type="password" onChange={(e) => { setPassword(e.target.value) }} />
-        </div>
-        <a href="/signup" style={{ margin: "0px" }}>Don't have account ? Create a Account</a>
-        <div className="buttonclass">
-          <button className="btnclass">
-            {
-              issumbit ?
-                <>
-                  <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                  &nbsp; logging in
-                </>
-                : "Log in"
-            }
-          </button>
-        </div>
-      </form>
-    </div>
-      )
-    }
+      {isLoading ? <Loader /> :
+        (
+          <div className="loginpage">
+            <img src="/media/images/login.png" alt="" />
+            <form onSubmit={Loginhandle}>
+              <h1 style={{ textAlign: "center", marginBottom: "1rem" }}>Login In Blogmenia</h1>
+              <div className="input-container">
+                <label >Email</label>
+                <input type="text" onChange={(e) => { setEmail(e.target.value) }} />
+              </div>
+              <div className="input-container">
+                <label>Password</label>
+                <input type="password" onChange={(e) => { setPassword(e.target.value) }} />
+              </div>
+              <a href="/signup" style={{ margin: "0px" }}>Don't have account ? Create a Account</a>
+              <div className="buttonclass">
+                <button className="btnclass">Log in</button>
+              </div>
+            </form>
+          </div>
+        )
+      }
     </>
 
   );
